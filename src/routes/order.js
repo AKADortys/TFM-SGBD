ad6a3f1a27d7b5ea7955546c1ec0bb6ba9a7b1c7
AKADortys <thibault.a@outlook.com>
2025-02-05 13:45:14 +0100

    ajout mineur mdw route order

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/order.controller");
+const tokenMdw = require("../middlewares/jwt.middleware");
+const permissionsMdw = require("../middlewares/permissions.middleware");
 
-router.get("/", orderController.getAllOrders);
-router.get("/:id", orderController.getOrderById);
+router.use(tokenMdw);
+
+router.get("/", permissionsMdw, orderController.getAllOrders);
+router.get("/:id", permissionsMdw, orderController.getOrderById);
 router.post("/", orderController.createOrder);
 router.put("/:id", orderController.updateOrder);
 router.delete("/:id", orderController.deleteOrder);
