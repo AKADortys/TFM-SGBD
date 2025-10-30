fc2b065323e8c34200b530caee318a69af12f2d0
AKADortys <thibault.a@outlook.com>
2025-10-30 10:27:54 +0100

    feat(logging): implement Winston logger for improved logging functionality

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const connect = require("./config/db");
 const { swaggerUi, specs } = require("./config/swagger");
+const logger = require("./utils/logger.util");
+
 // variables d'environnement
 dotenv.config();
+
 const app = express();
+
 // Connexion à la base de données
 connect();
+
 // config des middlewares
 app.use(cookieParser());
 app.use(
@@ -19,13 +24,20 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`);
+  next();
+});
+
 // Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
 // Routes
 app.use("/users", require("./routes/users"));
 app.use("/auth", require("./routes/authentification"));
 app.use("/products", require("./routes/products"));
 app.use("/orders", require("./routes/order"));
+
 // Démarrage du serveur
 const PORT = process.env.APP_PORT || 5000;
 app.listen(PORT, () => {
